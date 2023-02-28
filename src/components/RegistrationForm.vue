61ffa547a3643cd6178a08088230c606d29668e6
rodriguestg <grs.ibr@gmail.com>
2023-02-28 16:24:50 -0300

    Configuração e validação do cadastro de produtos, mock de produtos padrões

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -13,36 +13,83 @@
       <option>Sim</option>
       <option>Não</option>
     </select>
-    <button class="btn btn-primary" type="button" v-on:click="registerProduct()">Cadastrar</button>
+    <button class="btn btn-primary" type="button" v-bind:disabled="formValidation()"
+      v-on:click="registerProduct()">Cadastrar</button>
   </form>
-  <div v-show="this.products.length > 0">
-    {{ this.products }}
+  <div class="container text-center" v-show="this.products.length > 0">
+    <table class="table">
+      <thead>
+        <tr class="row align-items-center">
+          <td class="col">Id</td>
+          <td class="col">Nome</td>
+          <td class="col">Ativo</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="row align-items-center" v-for="product in products" v-bind:key="product.id">
+          <td class="col">{{ product.id }}</td>
+          <td class="col">{{ product.nome }}</td>
+          <td class="col">{{ product.status }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+  <p class="col" v-show="this.products.length === 0">Não existem produtos</p>
 </template>
 
 <script>
+import productsMock from '../components/dataMocks/products.mock'
+
 export default {
   name: 'RegistrationForm',
   props: {
     msg: String
   },
+
+  // created() {
+  //   const dataProducts = JSON.parse(localStorage.getItem('products'));
+  //   if (dataProducts) {
+  //     this.products = dataProducts;
+  //   }
+  // },
+
   data() {
     return {
-      products: [
-      ],
+      products: [...productsMock],
+      id: 3,
       nome: '',
       status: '',
     }
   },
+
+
   methods: {
     registerProduct() {
 
       this.products.push({
+        id: this.id + 1,
         nome: this.nome,
         status: this.status,
       })
 
-    }
+      const lastProduct = this.products.at(-1);
+      this.id = lastProduct.id;
+
+      this.nome = '';
+      this.status = '';
+    },
+
+    // saveProducts() {
+    //   localStorage.setItem('products', JSON.stringify([...this.products]))
+    // },
+
+    formValidation() {
+      if (this.nome.length > 1 && this.status.length !== 0) {
+        return false;
+      } else {
+        return true;
+      }
+    },
 
   }
 }
---
+++ src/components/dataMocks/products.mock.js
@@ -0,0 +1,7 @@
+const productsMock = [
+  { id: 1, nome: 'PROSPECÇÃO/MAILING', status: 'Sim' },
+  { id: 2, nome: 'ENRIQUECIMENTO DE DADOS', status: 'Sim' },
+  { id: 3, nome: 'CONSULTA DE DADOS', status: 'Sim' },
+]
+
+export default productsMock;
