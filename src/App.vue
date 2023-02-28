5df94bd57da3bff3e4af330e94993e72c0c47e9f
rodriguestg <grs.ibr@gmail.com>
2023-02-28 04:19:10 -0300

    Criação dos inputs de cadastro de produtos e organização inicial do component Register

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App!!!"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App!!!"/> -->
+  <RegistrationForm msg="Cadastro de Produtos" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import RegistrationForm from './components/RegistrationForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    RegistrationForm
   }
 }
 </script>
---
+++ src/components/RegistrationForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <form class="form-group">
+    <h1>{{ msg }}</h1>
+    <label for="product-name" class="form-label">
+      Nome do produto:
+    </label>
+    <input id="product-name" placeholder="Produto" name="Nome do Produto" class="form-control" v-model="nome" />
+    <b></b>
+    <label for="active" class="form-label">
+      Ativo:
+    </label>
+    <select id="active" class="form-control" v-model="status">
+      <option>Sim</option>
+      <option>Não</option>
+    </select>
+    <button class="btn btn-primary" type="button" v-on:click="registerProduct()">Cadastrar</button>
+  </form>
+  <div v-show="this.products.length > 0">
+    {{ this.products }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegistrationForm',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      products: [
+      ],
+      nome: '',
+      status: '',
+    }
+  },
+  methods: {
+    registerProduct() {
+
+      this.products.push({
+        nome: this.nome,
+        status: this.status,
+      })
+
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
